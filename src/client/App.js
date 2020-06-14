cea1efc91747a5268eea96b0562bf08937513fce
Efim Pushkarev <pushkarevev@yandex.ru>
2020-06-14 19:46:50 +0300

    Added components and styles for them. Otherwise added search form and header

--- src/client/App.js
+++ src/client/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { BrowserRouter, useHistory, Link, Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Repository from './components/Repository';
+import MainPage from './components/MainPage';
 
 export default function App(props) {
     let history = useHistory();
     return (
-        <></>
+        <BrowserRouter history={history}>
+            <Header />
+            <Switch>
+                <Route path={'/'} component={MainPage} />
+                <Route path={'/test'} component={Repository} />
+            </Switch>
+        </BrowserRouter>
     );
 }
---
+++ src/client/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header(props) {
+    return (
+        <div id={'header'}>
+            <p>Пушкарев тестовое задание для Frontend-стажера (Github Dashboard)</p>
+        </div>
+    )
+}
