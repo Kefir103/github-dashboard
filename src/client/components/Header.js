82c8d2f85226d47f210d3d74d462ba9ff46fece1
Efim Pushkarev <pushkarevev@yandex.ru>
2020-06-14 19:48:16 +0300

    Changed title in Header.js

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header(props) {
     return (
         <div id={'header'}>
-            <p>Пушкарев тестовое задание для Frontend-стажера (Github Dashboard)</p>
+            <p>Пушкарев Е.В. | Тестовое задание для Frontend-стажера (Github Dashboard)</p>
         </div>
     )
 }
