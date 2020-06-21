3be6108fa892c88aab0c1e1740cc23a33f10425c
Efim Pushkarev <pushkarevev@yandex.ru>
2020-06-22 01:00:59 +0300

    Добавлен поиск репозиториев, убраны из localStorage уже загруженные репозитории

--- src/client/components/main_page/MainPage.js
+++ src/client/components/main_page/MainPage.js
@@ -7,42 +7,70 @@ export default class MainPage extends Component {
 
         this.state = {
             repos: [],
+            searchText: '',
         };
+
+        this.handleSearchInputChange = this.handleSearchInputChange.bind(this);
+        this.handleSubmitClick = this.handleSubmitClick.bind(this);
     }
 
-    fillByEmptySearchField(page) {
+    handleSearchInputChange(event) {
+        event.preventDefault();
+        this.setState({
+            searchText: event.target.value,
+        });
+    }
+
+    handleSubmitClick(event) {
+        event.preventDefault();
+        if (this.state.searchText) {
+            localStorage.setItem('search', this.state.searchText);
+        } else {
+            localStorage.removeItem('search');
+        }
+        this.searchRepos(localStorage.getItem('page'), false);
+    }
+
+    searchRepos(page) {
         if (!page) {
             page = 1;
         }
-        fetch(
-            `https://api.github.com/search/repositories?q=stars:>1&sort=stars&order=desc&page=${page}&per_page=10`,
-            {
-                method: 'GET',
-            }
-        )
+
+        let url = `https://api.github.com/search/repositories?q=${localStorage.getItem(
+            'search'
+        )}&sort=stars&order=desc&page=${page}&per_page=10`;
+
+        if (!localStorage.getItem('search')) {
+            url = `https://api.github.com/search/repositories?q=stars:>1&sort=stars&order=desc&page=${page}&per_page=10`;
+        }
+
+        fetch(url, {
+            method: 'GET',
+        })
             .then((response) => response.json())
             .then((result) => {
+                this.setState({
+                    repos: result.items,
+                });
                 localStorage.setItem('page', page.toString());
-                localStorage.setItem('searchingRepos', JSON.stringify(result.items));
             });
     }
 
     componentDidMount() {
-        if (!localStorage.getItem('page') || !localStorage.getItem('searchingRepos')) {
-            this.fillByEmptySearchField(localStorage.getItem('page'));
-        } else {
-            this.setState({
-                repos: JSON.parse(localStorage.getItem('searchingRepos')),
-            });
-        }
+        this.searchRepos(localStorage.getItem('page'));
     }
 
     render() {
         return (
             <div>
                 <form id={'search-form'}>
-                    <input type={'search'} placeholder={'Введите имя репозитория'} />
-                    <button type={'submit'} />
+                    <input
+                        type={'search'}
+                        placeholder={'Введите имя репозитория'}
+                        onChange={this.handleSearchInputChange}
+                        defaultValue={localStorage.getItem('search')}
+                    />
+                    <button type={'submit'} onClick={this.handleSubmitClick}/>
                 </form>
                 {this.state.repos
                     ? this.state.repos.map((repository) => <ListItem repository={repository} />)
