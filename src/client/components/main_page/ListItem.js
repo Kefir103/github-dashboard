94c1788b59057051cb00104f51bdc88c5b10909b
Efim Pushkarev <pushkarevev@yandex.ru>
2020-06-22 01:02:55 +0300

    Добавлено склонение по падежам в списке репозиториев

--- src/client/components/main_page/ListItem.js
+++ src/client/components/main_page/ListItem.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import history from '../../history';
+import { num2str } from '../../localeFunctions';
 
 export default function ListItem(props) {
     return (
         <div
             className={'list-item'}
             onClick={() => {
-                history.push(`/repos/${props.repository.name}`, {...props.repository});
+                history.push(`/repos/${props.repository.name}`, { ...props.repository });
             }}>
             <h4>
-                {props.repository.name} ({props.repository.stargazers_count} звезд)
+                {props.repository.name} ({props.repository.stargazers_count}{' '}
+                {num2str(props.repository.stargazers_count, ['звезда', 'звезды', 'звезд'])}
+                )
             </h4>
             <p>Последнее изменение: {new Date(props.repository.pushed_at).toLocaleString()}</p>
-            <p>URL: <a href={props.repository.html_url}>{props.repository.html_url}</a></p>
+            <p>
+                URL: <a href={props.repository.html_url}>{props.repository.html_url}</a>
+            </p>
         </div>
     );
 }
