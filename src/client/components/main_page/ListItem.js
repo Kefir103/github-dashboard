40b1ec99ef1574f49d7f79e58b0829ffef609953
Efim Pushkarev <pushkarevev@yandex.ru>
2020-06-22 00:41:19 +0300

    Сделал ссылку на гитхаб ссылкой, подкорректировал стили для списка репозиториев

--- src/client/components/main_page/ListItem.js
+++ src/client/components/main_page/ListItem.js
@@ -12,7 +12,7 @@ export default function ListItem(props) {
                 {props.repository.name} ({props.repository.stargazers_count} звезд)
             </h4>
             <p>Последнее изменение: {new Date(props.repository.pushed_at).toLocaleString()}</p>
-            <p>URL: {props.repository.html_url}</p>
+            <p>URL: <a href={props.repository.html_url}>{props.repository.html_url}</a></p>
         </div>
     );
 }
