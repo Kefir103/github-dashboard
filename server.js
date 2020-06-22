9eda38f5f60ceecb1d14165da5a8c828826838be
Efim Pushkarev <pushkarevev@yandex.ru>
2020-06-22 16:47:58 +0300

    Проект собирается и запускается с помощью npm start. Также прикручен history api fallback к серверу, чтобы не выкидывало ошибки при перезагрузки страницы карточки репозитория

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const path = require('path');
+const fallback = require('express-history-api-fallback');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static('build'));
 app.use(express.static('public'));
+app.use(fallback('index.html', { root: path.join(__dirname, 'build') }));
 
 app.get('/', (req, res) => {
-    res.sendFile('/build/index.html');
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
