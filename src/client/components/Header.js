b47fe5940da4f6bbc1f8dee4915081fc240e036b
Efim Pushkarev <pushkarevev@yandex.ru>
2020-07-23 21:13:32 +0300

    Добавил ссылку на репозиторий проекта

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Header(props) {
     return (
         <header>
             <h1>Github Dashboard</h1>
+            <a href={'https://github.com/Kefir103/github-dashboard'} className={'contact-links'} target={'_blank'}>
+                <FontAwesomeIcon icon={faGithub} size={'2x'} color={'whitesmoke'} />
+            </a>
         </header>
     );
 }
